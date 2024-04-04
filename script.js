2e1a00fe3143bf30c85e8fb6e44ab66b37062f63
Mathews Baio <baiomathews@gmail.com>
2024-04-04 17:33:24 -0300

    Concentration Card Game

---
+++ script.js
@@ -0,0 +1,50 @@
+
+let  arrayLetters = ["C", "D", "B", "C", "E", "D", "A", "E", "B", "A"];
+
+
+let cards = document.querySelectorAll(".card");
+let count = 1
+let lastcard = 1;
+let newDupla = true
+cards.forEach(card =>{
+    card.addEventListener("click", event =>{
+        if(count == 1) {
+            newDupla = false
+            card.textContent = `${arrayLetters[card.getAttribute("posicao")]}`;
+            card.classList.add("A"); 
+            lastcard = card.getAttribute("posicao")
+            count++;
+        } else if (count == 2) {
+            if(arrayLetters[card.getAttribute("posicao")] == arrayLetters[lastcard]){
+                card.textContent = `${arrayLetters[card.getAttribute("posicao")]}`;
+                card.classList.add("A");
+                count = 1;
+                newDupla = true;
+            } else{
+                    if (!newDupla) {
+                    card.textContent = `${arrayLetters[card.getAttribute("posicao")]}`
+                    card.classList.add("B")
+                    setTimeout(resetCards, 1000)
+                    }
+                
+            }
+        }
+    })
+})
+
+function resetCards() {
+    cards.forEach(card => {
+        card.textContent = "X";
+        card.classList.remove("A")
+        card.classList.remove("B")
+        count = 1
+    })
+}
+
+
+
+
+
+
+
+
